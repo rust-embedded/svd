cf0cff982372d4c670bb1ca12e76cf82b5c9c3cd
Jorge Aparicio <japaricious@gmail.com>
2016-07-23 15:16:06 -0500

    test on all the channels

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,6 +1,6 @@
 set -ex
 
-if [[ "$TRAVIS_PULL_REQUEST" = 'false' ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" = 'false' && "$TRAVIS_RUST_VERSION" = 'stable' ]]; then
     ./ghp-import/ghp_import.py target/doc
     set +x
     git push -fq "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages && echo OK
