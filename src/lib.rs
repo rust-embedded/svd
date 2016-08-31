f49a81e5864e437fd8dd5ee9511ba34f448d1486
Brandon Edens <brandonedens@gmail.com>
2016-08-31 09:05:14 -0700

    Add <access> parsing to enumerated value.

--- src/lib.rs
+++ src/lib.rs
@@ -190,6 +190,7 @@ pub struct Field {
     pub name: String,
     pub description: Option<String>,
     pub bit_range: BitRange,
+    pub access: Option<Access>,
     pub enumerated_values: Option<EnumeratedValues>,
 }
 
@@ -201,6 +202,7 @@ impl Field {
             name: try!(tree.get_child_text("name")),
             description: tree.get_child_text("description"),
             bit_range: BitRange::parse(tree),
+            access: tree.get_child("access").map(Access::parse),
 
             enumerated_values:
                 tree.get_child("enumeratedValues").map(EnumeratedValues::parse),
