bdfa72745b54b2a760c4747c93d0ffe4b4d814cd
Emil Gardström <emil.gardstrom@gmail.com>
2018-03-18 07:32:48 +0100

    Use or_else to avoid calling clone when it's uneccessary

--- src/lib.rs
+++ src/lib.rs
@@ -160,9 +160,9 @@ pub struct Peripheral {
 impl Peripheral {
     pub fn derive_from(&self, other: &Peripheral) -> Peripheral {
         let mut derived = self.clone();
-        derived.group_name = derived.group_name.or(other.group_name.clone());
-        derived.description = derived.description.or(other.description.clone());
-        derived.registers = derived.registers.or(other.registers.clone());
+        derived.group_name = derived.group_name.or_else(|| other.group_name.clone());
+        derived.description = derived.description.or_else(|| other.description.clone());
+        derived.registers = derived.registers.or_else(|| other.registers.clone());
         if derived.interrupt.is_empty() {
             derived.interrupt = other.interrupt.clone();
         }
