325886123f97a485af86423af0738f39abe0ed51
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-05-12 18:38:38 +0300

    access & usage rename when serialize

--- src/svd/access.rs
+++ src/svd/access.rs
@@ -1,9 +1,14 @@
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Access {
+    #[cfg_attr(feature = "serde", serde(rename = "read-only"))]
     ReadOnly,
+    #[cfg_attr(feature = "serde", serde(rename = "read-write"))]
     ReadWrite,
+    #[cfg_attr(feature = "serde", serde(rename = "read-writeOnce"))]
     ReadWriteOnce,
+    #[cfg_attr(feature = "serde", serde(rename = "writeOnce"))]
     WriteOnce,
+    #[cfg_attr(feature = "serde", serde(rename = "write-only"))]
     WriteOnly,
 }
