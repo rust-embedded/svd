1dd6b0803a979e7e8361e50b815bc5bed478563a
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-05-12 18:29:32 +0300

    serializers for Cluster, Register and Field

--- src/svd/cluster.rs
+++ src/svd/cluster.rs
@@ -2,7 +2,6 @@ use core::ops::{Deref, DerefMut};
 
 use crate::svd::{clusterinfo::ClusterInfo, dimelement::DimElement};
 
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[derive(Clone, Debug, PartialEq)]
 pub enum Cluster {
     Single(ClusterInfo),
@@ -25,6 +24,52 @@ impl DerefMut for Cluster {
         match self {
             Cluster::Single(info) => info,
             Cluster::Array(info, _) => info,
+        }
+    }
+}
+
+#[cfg(feature = "serde")]
+mod ser_de {
+    use super::*;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    #[derive(serde::Deserialize, serde::Serialize)]
+    struct ClusterArray {
+        #[cfg_attr(feature = "serde", serde(flatten))]
+        #[cfg_attr(feature = "serde", serde(default))]
+        #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+        dim: Option<DimElement>,
+        #[cfg_attr(feature = "serde", serde(flatten))]
+        info: ClusterInfo,
+    }
+
+    impl Serialize for Cluster {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            match self {
+                Cluster::Single(info) => info.serialize(serializer),
+                Cluster::Array(info, dim) => ClusterArray {
+                    dim: Some(dim.clone()),
+                    info: info.clone(),
+                }
+                .serialize(serializer),
+            }
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Cluster {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let ClusterArray { dim, info } = ClusterArray::deserialize(deserializer)?;
+            if let Some(dim) = dim {
+                Ok(Cluster::Array(info, dim))
+            } else {
+                Ok(Cluster::Single(info))
+            }
         }
     }
 }
