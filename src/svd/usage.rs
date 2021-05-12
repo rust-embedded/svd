b1b715089738be31a33a78cddf3f528637c7cec2
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-05-12 19:27:17 +0300

    usage rename

--- src/svd/usage.rs
+++ src/svd/usage.rs
@@ -1,4 +1,5 @@
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Usage {
     Read,
