52bccd04ed864b46d2001f7e1cacccd27e85a908
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-08-19 10:40:25 +0300

    Endian

--- svd-rs/src/endian.rs
+++ svd-rs/src/endian.rs
@@ -1,8 +1,27 @@
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Endian {
     Little,
     Big,
     Selectable,
     Other,
+}
+
+impl Default for Endian {
+    fn default() -> Self {
+        Self::Little
+    }
+}
+
+impl Endian {
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "little" => Some(Endian::Little),
+            "big" => Some(Endian::Big),
+            "selectable" => Some(Endian::Selectable),
+            "other" => Some(Endian::Other),
+            _ => None,
+        }
+    }
 }
