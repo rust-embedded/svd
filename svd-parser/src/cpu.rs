8e6ecbae46ec28ec02e7ee354756d743f2a7d791
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-11-09 13:35:03 +0300

    add missing entries to Cpu

--- svd-parser/src/cpu.rs
+++ svd-parser/src/cpu.rs
@@ -1,5 +1,6 @@
 use super::*;
 use crate::svd::{Cpu, Endian};
+use crate::types::BoolParse;
 
 impl Parse for Cpu {
     type Object = Self;
@@ -17,8 +18,17 @@ impl Parse for Cpu {
             .endian(Endian::parse(&tree.get_child_elem("endian")?, config)?)
             .mpu_present(tree.get_child_bool("mpuPresent")?)
             .fpu_present(tree.get_child_bool("fpuPresent")?)
+            .fpu_double_precision(optional::<BoolParse>("fpuDP", tree, &())?)
+            .dsp_present(optional::<BoolParse>("dspPresent", tree, &())?)
+            .icache_present(optional::<BoolParse>("icachePresent", tree, &())?)
+            .dcache_present(optional::<BoolParse>("dcachePresent", tree, &())?)
+            .itcm_present(optional::<BoolParse>("itcmPresent", tree, &())?)
+            .dtcm_present(optional::<BoolParse>("dtcmPresent", tree, &())?)
+            .vtor_present(optional::<BoolParse>("vtorPresent", tree, &())?)
             .nvic_priority_bits(tree.get_child_u32("nvicPrioBits")?)
             .has_vendor_systick(tree.get_child_bool("vendorSystickConfig")?)
+            .device_num_interrupts(optional::<u32>("deviceNumInterrupts", tree, &())?)
+            .sau_num_regions(optional::<u32>("sauNumRegions", tree, &())?)
             .build(config.validate_level)
             .map_err(|e| SVDError::from(e).at(tree.id()))
     }
