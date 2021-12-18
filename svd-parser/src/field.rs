4d0c2ddaffc17c807446da5c75d45c42c501ec79
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-12-18 18:18:58 +0300

    parse array

--- svd-parser/src/field.rs
+++ svd-parser/src/field.rs
@@ -1,6 +1,5 @@
 use super::*;
-use crate::svd::{DimElement, Field, FieldInfo};
-use crate::{elementext::ElementExt, Config};
+use crate::svd::Field;
 
 impl Parse for Field {
     type Object = Self;
@@ -8,27 +7,6 @@ impl Parse for Field {
     type Config = Config;
 
     fn parse(tree: &Node, config: &Self::Config) -> Result<Self, Self::Error> {
-        if !tree.has_tag_name("field") {
-            return Err(SVDError::NotExpectedTag("field".to_string()).at(tree.id()));
-        }
-
-        let info = FieldInfo::parse(tree, config)?;
-
-        if tree.get_child("dimIncrement").is_some() {
-            let array_info = DimElement::parse(tree, config)?;
-            check_has_placeholder(&info.name, "field").map_err(|e| e.at(tree.id()))?;
-            if let Some(indexes) = &array_info.dim_index {
-                if array_info.dim as usize != indexes.len() {
-                    return Err(SVDError::IncorrectDimIndexesCount(
-                        array_info.dim as usize,
-                        indexes.len(),
-                    )
-                    .at(tree.id()));
-                }
-            }
-            Ok(info.array(array_info))
-        } else {
-            Ok(info.single())
-        }
+        parse_array("field", tree, config)
     }
 }
